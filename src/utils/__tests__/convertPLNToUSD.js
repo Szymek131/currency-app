0d6199bcc9ac79463cf5e2486dddd6b27446664a
Szymek131 <sz.wiktor131@gmail.com>
2023-04-20 17:33:14 +0200

    add first tests

---
+++ src/utils/__tests__/convertPLNToUSD.js
@@ -0,0 +1,29 @@
+import { convertPLNToUSD } from './../convertPLNtoUSD';
+
+describe('ConvertPLNtoUSD', () => {
+  it('should return proper value when good input', () => {
+    expect(convertPLNToUSD(1)).toBe('$0.29');
+    expect(convertPLNToUSD(2)).toBe('$0.57');
+    expect(convertPLNToUSD(20)).toBe('$5.71');
+    expect(convertPLNToUSD(12)).toBe('$3.43');
+  });
+  it('should return NaN when input is text', () => {
+    expect(convertPLNToUSD('1')).toBeNaN();
+    expect(convertPLNToUSD('asd')).toBeNaN();
+    expect(convertPLNToUSD('-123asczx')).toBeNaN();
+  });
+  it('should return NaN when input is empty', () => {
+    expect(convertPLNToUSD()).toBeNaN();
+  });
+  it('should return "Error" when input is not string or number', () => {
+    expect(convertPLNToUSD([])).toBe('Error');
+    expect(convertPLNToUSD({})).toBe('Error');
+    expect(convertPLNToUSD(null)).toBe('Error');
+    expect(convertPLNToUSD(function () { })).toBe('Error');
+  });
+  it('should return $0.00 when input is less than 0', () => {
+    expect(convertPLNToUSD(-1)).toBe('$0.00');
+    expect(convertPLNToUSD(-6)).toBe('$0.00');
+    expect(convertPLNToUSD(-1234)).toBe('$0.00');
+  });
+});
--- src/utils/convertPLNToUSD.js
+++ src/utils/convertPLNToUSD.js
@@ -1,7 +1,16 @@
 export const convertPLNToUSD = (PLN) => {
 
   const PLNtoUSD = PLN / 3.5;
-  
+
+  if (typeof PLN === 'string' ||
+    typeof PLN === 'undefined') {
+    return NaN;
+  } else if (PLN < 0) {
+    return '$0.00'
+  } else if (typeof PLN !== 'number' && typeof PLN !== 'string') {
+    return 'Error'
+  }
+
   const formatter = new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD'
